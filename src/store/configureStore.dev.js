f11d2f7f597855c576aebd3626ec72de6b39f6ea
ddrevnov <ddrievnov@mail.ru>
2017-02-04 23:42:00 +0200

    add redux-logger middleware

--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -3,11 +3,12 @@ import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import logger from 'redux-logger';
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
-    composeWithDevTools(applyMiddleware(thunk, reduxImmutableStateInvariant())),
+    composeWithDevTools(applyMiddleware(thunk, logger(), reduxImmutableStateInvariant())),
   );
 }
